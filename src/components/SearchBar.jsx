1bc83128db68a0b8de5ccf54cc7ac5ca577d1435
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 14:36:28 +0300

    add handle change

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import {useState} from 'react'
 import { Paper, TextField } from '@mui/material'
 
 function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const handleChange = (event) => { 
+        setSearchTerm(event.target.value)
+    }
+
+    const onKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            console.log(searchTerm)
+        }
+    }
   return (
       <Paper elevation={6} style={{padding: "25px"}}>
           <TextField
               fullWidth
               label="Search..."
-              value={ }
-              onChange={ }
-              onKeyPress = { }
+              value={ searchTerm }
+              onChange={ handleChange }
+              onKeyPress = { onKeyPress }
           />
     </Paper>
   )
