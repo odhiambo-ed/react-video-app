b79a232cc66242d11b9e5efcf17d9ab1640bcff7
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 19:13:45 +0300

    update api js

--- src/api/youTube.js
+++ src/api/youTube.js
@@ -1,21 +1,25 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiKey = process.env.GOOGLE_API_KEY;
 
 const api = axios.create({
-    baseURL: "https://www.googleapis.com/youtube/v3",
+    baseURL: 'https://www.googleapis.com/youtube/v3',
 });
 
-export const getYouTubeVideos = async (searchTerm) => { 
-    const response = await api.get('/search', {
-        params: {
-            part: 'snippet',
-            maxResults: 5,
-            key: apiKey,
-            q: searchTerm,
-            type: 'video',
-        }
-    });
-    return response.data;
-}
-
+export const getYouTubeVideos = async (searchTerm) => {
+    try {
+        const response = await api.get('/search', {
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: apiKey,
+                q: searchTerm,
+                type: 'video',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching YouTube videos:', error);
+        throw error;
+    }
+};
