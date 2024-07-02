b6c7b013286c0d0269af085440a7f95adb376bc4
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 19:02:55 +0300

    add getYouTubeVideos

--- src/api/youTube.js
+++ src/api/youTube.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
-export default axios.create({
+const apiKey = process.env.GOOGLE_API_KEY;
+
+const api = axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
-    params: {
-        part: "snippet",
-        maxResults: 5,
-        key: process.env.GOOGLE_API_KEY,
-    },
 });
+
+export const getYouTubeVideos = async (searchTerm) => { 
+    const response = await api.get('/search', {
+        params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: apiKey,
+            q: searchTerm,
+            type: 'video',
+        }
+    });
+    return response.data;
+}
+
