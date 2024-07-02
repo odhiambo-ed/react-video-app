7e307acdaa891816f84005346bce46c6b717af22
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 19:02:15 +0300

    add onSubmit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Grid from '@mui/material/Grid';
 import SearchBar from './components/SearchBar';
+import { getYouTubeVideos } from './api/youTube';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Grid item xs={11}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
-            <SearchBar />
+            <SearchBar onSubmit = {getYouTubeVideos} />
           </Grid>
           {/* <Grid>{VideoDetail}</Grid>
           <Grid>{VideoList}</Grid> */}
