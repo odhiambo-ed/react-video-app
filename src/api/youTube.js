88d4058842662658cf4565076ab9ae08d8d0e6e7
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 18:02:38 +0300

    update api

--- src/api/youTube.js
+++ src/api/youTube.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3",
-    // params: {
-    //     part: "snippet",
-    //     maxResults: 5,
-    //     key: process.env.REACT_APP_YOUTUBE_API_KEY,
-    // },
+    params: {
+        part: "snippet",
+        maxResults: 5,
+        key: process.env.GOOGLE_API_KEY,
+    },
 });
