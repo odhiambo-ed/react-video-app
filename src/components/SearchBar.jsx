b4cd59f98d51f5427122ee7fc632abf60f4532ab
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 19:11:27 +0300

    update search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,30 @@
-import {useState} from 'react'
-import { Paper, TextField } from '@mui/material'
+import { useState } from "react";
+import { Paper, TextField } from "@mui/material";
 
-function SearchBar(props) {
-    const { onsubmit } = props;
-    const [searchTerm, setSearchTerm] = useState('')
+function SearchBar({ onSubmit }) {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleChange = (event) => { 
-        setSearchTerm(event.target.value)
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onSubmit(searchTerm);
     }
+  };
 
-    const handleKeyPress = (event) => {
-        if (event.key === 'Enter') {
-            onsubmit(searchTerm)
-        }
-    }
   return (
-      <Paper elevation={6} style={{padding: "25px"}}>
-          <TextField
-              fullWidth
-              label="Search..."
-              value={ searchTerm }
-              onChange={ handleChange }
-              onKeyDown = { handleKeyPress }
-          />
+    <Paper elevation={6} style={{ padding: "25px" }}>
+      <TextField
+        fullWidth
+        label="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
     </Paper>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
