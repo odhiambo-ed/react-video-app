3d2e626244fa7b836946d092853de2aba2253ac1
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-02 14:24:02 +0300

    import search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Grid from '@mui/material/Grid';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <Grid style={{ justifyContent: "center" }} container spacing={10}>
       <Grid item xs={11}>
         <Grid container spacing={10}>
-          {/* <Grid>{SearchBar}</Grid>
-          <Grid>{VideoDetail}</Grid>
+          <Grid>{SearchBar}</Grid>
+          {/* <Grid>{VideoDetail}</Grid>
           <Grid>{VideoList}</Grid> */}
         </Grid>
       </Grid>
