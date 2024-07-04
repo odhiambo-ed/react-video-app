a68f3721b29e9c5de46326e538a6acc33b046572
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-04 23:41:02 +0300

    add grid to video item

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Grid, Paper, Typography} from '@mui/material'
 
 function VideoItem() {
   return (
-    <div>VideoItem</div>
+      <Grid item xs={12}>
+        <Paper style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => onSelectVideo(video)} >
+          <img alt="thumbnail" style={{ marginRight: '20px' }} />
+          <Typography variant="subtitle1"><b>Title</b></Typography>
+        </Paper>
+    </Grid>
   )
 }
 
