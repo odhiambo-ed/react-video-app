5f27764ef406896f7b3bd52f640e3b3fd642256c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-07-04 23:43:30 +0300

    add img source

--- src/components/VideoItem.jsx
+++ src/components/VideoItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Grid, Paper, Typography} from '@mui/material'
 
-function VideoItem() {
+function VideoItem({ video, onSelectVideo}) {
   return (
       <Grid item xs={12}>
         <Paper style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }} onClick={() => onSelectVideo(video)} >
-          <img alt="thumbnail" style={{ marginRight: '20px' }} />
+          <img alt="thumbnail" style={{ marginRight: '20px' }} src={video.snippet.thumbnails.medium.url} />
           <Typography variant="subtitle1"><b>Title</b></Typography>
         </Paper>
     </Grid>
